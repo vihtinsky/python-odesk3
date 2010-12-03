456e55717fbc71278edf74a5161d7fa412e4f43e
Chirag B. Jadwani <chirag.jadwani@gmail.com>
2010-12-03 20:20:01 +0530

    r378 added provider get affiliate test

--- odesk/tests.py
+++ odesk/tests.py
@@ -774,6 +774,8 @@ def test_provider():
     assert pr.update_quickinfo(1, {'quickinfo':'quickinfo'}) == provider_dict,\
         pr.update_quickinfo(1, {'quickinfo':'quickinfo'})
 
+    result = pr.get_affiliates(1)
+    assert result == provider_dict['profile']
 
 trays_dict = {'trays': [{u'unread': u'0', 
               u'type': u'sent', 
