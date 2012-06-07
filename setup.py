b6cecec949941fa2051fe883de14d09e88bdc5c2
Ilya Vihtinsky <vihtinsky@yandex.ru>
2012-06-07 13:08:00 +0400

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,19 @@ readme = open(os.path.join(os.path.dirname(__file__), 'README'))
 README = readme.read()
 readme.close()
 
-version = __import__('odesk').get_version()
+version = "0.1"
 
 
-setup(name='python-odesk',
+setup(name='python-odesk3',
       version=version,
-      description='Python bindings to oDesk API',
+      description='Python3 bindings to oDesk API',
       long_description=README,
       author='oDesk',
       author_email='python@odesk.com',
       maintainer='Volodymyr Hotsyk',
       maintainer_email='gotsyk@gmail.com',
-      dependency_links = ['https://github.com/hades/python-oauth2/tarball/python3#egg=python-oauth2',],
+      install_requires=['oauth2',],
+      dependency_links = ['https://github.com/hades/python-oauth2/tarball/python3#egg=oauth2',],
       packages=find_packages(),
       license = 'BSD',
       download_url ='http://github.com/odesk/python-odesk',
