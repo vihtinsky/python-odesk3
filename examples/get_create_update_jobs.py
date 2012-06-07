9c7b043b954e54208d4198a4f929ed05ae5fa26c
Ilya Vihtinsky <vihtinsky@yandex.ru>
2012-06-07 18:34:41 +0400

    Timereports and jobs examples

--- examples/get_create_update_jobs.py
+++ examples/get_create_update_jobs.py
@@ -12,17 +12,17 @@ SECRET_KEY = None
 #TODO: Desktop app example (check if it's working at all - wasn't last time)
 
 def hr_post_job(public_key, secret_key):
-    print "Emulating web-based app"
+    print("Emulating web-based app")
     #Instantiating a client without an auth token
     client = odesk.Client(public_key, secret_key)
-    print "Please to this URL (authorize the app if necessary):"
-    print client.auth.auth_url()
-    print "After that you should be redirected back to your app URL with " + \
-          "additional ?frob= parameter"
-    frob = raw_input('Enter frob: ') 
+    print("Please to this URL (authorize the app if necessary):")
+    print(client.auth.auth_url())
+    print("After that you should be redirected back to your app URL with " + \
+          "additional ?frob= parameter")
+    frob = input('Enter frob: ') 
     auth_token, user = client.auth.get_token(frob)
-    print "Authenticated user:"
-    print user
+    print("Authenticated user:")
+    print(user)
     #Instantiating a new client, now with a token. 
     #Not strictly necessary here (could just set `client.auth_token`), but 
     #typical for web apps, which wouldn't probably keep client instances 
@@ -39,17 +39,17 @@ def hr_post_job(public_key, secret_key):
                 'subcategory': 'Other - Web Development',
                 }
     try:
-        print client.hr.post_job(job_data)
-    except Exception, e:
-        print "Exception at %s %s" % (client.last_method, client.last_url)
+        print(client.hr.post_job(job_data))
+    except Exception as e:
+        print("Exception at %s %s" % (client.last_method, client.last_url))
         raise e
 
 
     
  
 if __name__ == '__main__':
-    public_key = PUBLIC_KEY or raw_input('Enter public key: ')
-    secret_key = SECRET_KEY or raw_input('Enter secret key: ')
+    public_key = PUBLIC_KEY or input('Enter public key: ')
+    secret_key = SECRET_KEY or input('Enter secret key: ')
 
     hr_post_job(public_key, secret_key)
 
--- examples/time_reports.py
+++ examples/time_reports.py
@@ -4,6 +4,7 @@ python-odesk version 0.1
 (C) 2010 oDesk
 """
 import odesk
+import odesk.utils
 from datetime import date
 
 PUBLIC_KEY = None
@@ -12,41 +13,41 @@ SECRET_KEY = None
 #TODO: Desktop app example (check if it's working at all - wasn't last time)
 
 def time_reports(public_key, secret_key):
-    print "Emulating web-based app"
+    print("Emulating web-based app")
     #Instantiating a client without an auth token
     client = odesk.Client(public_key, secret_key)
-    print "Please to this URL (authorize the app if necessary):"
-    print client.auth.auth_url()
-    print "After that you should be redirected back to your app URL with " + \
-          "additional ?frob= parameter"
-    frob = raw_input('Enter frob: ') 
+    print("Please to this URL (authorize the app if necessary):")
+    print(client.auth.auth_url())
+    print("After that you should be redirected back to your app URL with " + \
+          "additional ?frob= parameter")
+    frob = input('Enter frob: ')
     auth_token, user = client.auth.get_token(frob)
-    print "Authenticated user:"
-    print user
-    #Instantiating a new client, now with a token. 
-    #Not strictly necessary here (could just set `client.auth_token`), but 
-    #typical for web apps, which wouldn't probably keep client instances 
+    print("Authenticated user:")
+    print(user)
+    #Instantiating a new client, now with a token.
+    #Not strictly necessary here (could just set `client.auth_token`), but
+    #typical for web apps, which wouldn't probably keep client instances
     #between requests
     client = odesk.Client(public_key, secret_key, auth_token)
-    print client.time_reports.get_provider_report('user1', 
-                    odesk.Query(select=odesk.Query.DEFAULT_TIMEREPORT_FIELDS, 
-                    where=(odesk.Q('worked_on') <= date.today()) &\
-                     (odesk.Q('worked_on') > '2010-05-01')))
+    print(client.timereport.get_provider_report('user1',
+            odesk.utils.Query(select=odesk.utils.Query.DEFAULT_TIMEREPORT_FIELDS,
+            where=(odesk.utils.Q('worked_on') <= date.today()) &\
+             (odesk.utils.Q('worked_on') > '2012-05-01'))))
 
-    print client.time_reports.get_provider_report('user1', 
-                    odesk.Query(select=odesk.Query.DEFAULT_TIMEREPORT_FIELDS, 
-                    where=(odesk.Q('worked_on') <= date.today()) &\
-                     (odesk.Q('worked_on') > '2010-05-01')), hours=True)
+    print(client.timereport.get_provider_report('user1',
+            odesk.utils.Query(select=odesk.utils.Query.DEFAULT_TIMEREPORT_FIELDS,
+            where=(odesk.utils.Q('worked_on') <= date.today()) &\
+             (odesk.utils.Q('worked_on') > '2012-05-01')), hours=True))
 
-    print client.time_reports.get_agency_report('company1', 'agency1', 
-                    odesk.Query(select=odesk.Query.DEFAULT_TIMEREPORT_FIELDS, 
-                    where=(odesk.Q('worked_on') <= date.today()) &\
-                     (odesk.Q('worked_on') > '2010-05-01')), hours=True)
-    
- 
+    print(client.timereport.get_agency_report('company1', 'agency1',
+            odesk.utils.Query(select=odesk.utils.Query.DEFAULT_TIMEREPORT_FIELDS,
+            where=(odesk.utils.Q('worked_on') <= date.today()) &\
+            (odesk.utils.Q('worked_on') > '2010-05-01')), hours=True))
+
+
 if __name__ == '__main__':
-    public_key = PUBLIC_KEY or raw_input('Enter public key: ')
-    secret_key = SECRET_KEY or raw_input('Enter secret key: ')
+    public_key = PUBLIC_KEY or input('Enter public key: ')
+    secret_key = SECRET_KEY or input('Enter secret key: ')
 
     time_reports(public_key, secret_key)
 
