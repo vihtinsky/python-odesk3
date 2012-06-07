c36ae9d2f945573665ff639691a737934f643bcf
Ilya Vihtinsky <vihtinsky@yandex.ru>
2012-06-07 19:42:17 +0400

    Provider example

--- examples/provider.py
+++ examples/provider.py
@@ -9,50 +9,42 @@ from datetime import datetime
 PUBLIC_KEY = None
 SECRET_KEY = None
 
+
 #TODO: Desktop app example (check if it's working at all - wasn't last time)
 
 def provider(public_key, secret_key):
-    print "Emulating web-based app"
+    print("Emulating web-based app")
     #Instantiating a client without an auth token
     client = odesk.Client(public_key, secret_key)
-    print "Please to this URL (authorize the app if necessary):"
-    print client.auth.auth_url()
-    print "After that you should be redirected back to your app URL with " + \
-          "additional ?frob= parameter"
-    frob = raw_input('Enter frob: ') 
+    print("Please to this URL (authorize the app if necessary):")
+    print(client.auth.auth_url())
+    print("After that you should be redirected back to your app URL with " + \
+          "additional ?frob= parameter")
+    frob = input('Enter frob: ')
     auth_token, user = client.auth.get_token(frob)
-    print "Authenticated user:"
-    print user
-    #Instantiating a new client, now with a token. 
-    #Not strictly necessary here (could just set `client.auth_token`), but 
-    #typical for web apps, which wouldn't probably keep client instances 
+    print("Authenticated user:")
+    print(user)
+    #Instantiating a new client, now with a token.
+    #Not strictly necessary here (could just set `client.auth_token`), but
+    #typical for web apps, which wouldn't probably keep client instances
     #between requests
     client = odesk.Client(public_key, secret_key, auth_token)
-    # get skills
-    print "Provider skills:"
-    print client.provider.get_skills('~~someref')
-    # add new skill
-    print "Adding provider skill"
-    print client.provider.add_skill('~~someref', {'skill':'skill'})
-    # update a skill by giving a skill_id and new data
-    print "Updating provider skill"
-    print client.provider.update_skill('~~someref', 123, {'skill':'skill'})
-    # delete a skill by giving a skill_id
-    print "Deleting provider skill"
-    print client.provider.delete_skill('~~someref', 123)
-    # get quickinfo
-    print "Get quick info"
-    print client.provider.get_quickinfo('~~someref')
-    # update a quickinfo by giving new data
-    client.provider.update_quickinfo('~~someref', {'skill':'skill'})
-    print client.provider.get_affiliates('someref')
-    print "Revoke access"
-    print client.auth.revoke_token()    
+    print("Search providers:")
+    print(client.provider.get_providers({'q':'python'}))
+    print("Search jobs:")
+    print(client.provider.get_jobs({'q':'wowza'}))
+    print("Provider all:")
+    #someref is like 71de2d463c748623
+    print(client.provider.get_provider('~~someref'))
+    print("Provider brief:")
+    print(client.provider.get_provider_brief('~~someref'))
+    print("Revoke access")
+    print(client.auth.revoke_token())
 
- 
+
 if __name__ == '__main__':
-    public_key = PUBLIC_KEY or raw_input('Enter public key: ')
-    secret_key = SECRET_KEY or raw_input('Enter secret key: ')
+    public_key = PUBLIC_KEY or input('Enter public key: ')
+    secret_key = SECRET_KEY or input('Enter secret key: ')
 
     provider(public_key, secret_key)
 
