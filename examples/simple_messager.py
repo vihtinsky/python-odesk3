93adfaf72be659c79f0f20ae2b362c8de506de57
Ilya Vihtinsky <vihtinsky@yandex.ru>
2012-06-07 20:06:03 +0400

    simple_messager example

--- examples/simple_messager.py
+++ examples/simple_messager.py
@@ -12,33 +12,33 @@ SECRET_KEY = None
 #TODO: Desktop app example (check if it's working at all - wasn't last time)
 
 def simple_messager(public_key, secret_key):
-    print "Emulating web-based app"
+    print("Emulating web-based app")
     #Instantiating a client without an auth token
     client = odesk.Client(public_key, secret_key)
-    print "Please to this URL (authorize the app if necessary):"
-    print client.auth.auth_url()
-    print "After that you should be redirected back to your app URL with " + \
-          "additional ?frob= parameter"
-    frob = raw_input('Enter frob: ') 
+    print("Please to this URL (authorize the app if necessary):")
+    print(client.auth.auth_url())
+    print("After that you should be redirected back to your app URL with " + \
+          "additional ?frob= parameter")
+    frob = input('Enter frob: ')
     auth_token, user = client.auth.get_token(frob)
-    print "Authenticated user:"
-    print user
-    #Instantiating a new client, now with a token. 
-    #Not strictly necessary here (could just set `client.auth_token`), but 
-    #typical for web apps, which wouldn't probably keep client instances 
+    print("Authenticated user:")
+    print(user)
+    #Instantiating a new client, now with a token.
+    #Not strictly necessary here (could just set `client.auth_token`), but
+    #typical for web apps, which wouldn't probably keep client instances
     #between requests
     client = odesk.Client(public_key, secret_key, auth_token)
-    print client.mc.get_trays()
-    #print client.mc.get_tray_content('username', 'inbox')
-    #print client.mc.get_thread_content('username', '00')
-    print client.mc.post_message('username', 'username2', 'test from api', 'test body')
+    print(client.mc.get_trays())
+    #print(client.mc.get_tray_content('my_username', 'inbox'))
+    #print(client.mc.get_thread_content('my_username', '111111'))
+    print(client.mc.post_message('sender', 'recipient', 'test from api', 'test body'))
 
 
 
 
 if __name__ == '__main__':
-    public_key = PUBLIC_KEY or raw_input('Enter public key: ')
-    secret_key = SECRET_KEY or raw_input('Enter secret key: ')
+    public_key = PUBLIC_KEY or input('Enter public key: ')
+    secret_key = SECRET_KEY or input('Enter secret key: ')
 
     simple_messager(public_key, secret_key)
 
