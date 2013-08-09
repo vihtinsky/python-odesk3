fc3b8d2acd0cf260d5b34ff9bc7208d61644905c
Ilya Vihtinsky <vihtinsky@yandex.ru>
2013-08-09 14:24:08 +0400

    Modify setup.py for 2to3

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 import os
+import sys
+if sys.version_info[0] == 3:
+    import setuptools
+    if setuptools.__version__ < "0.7":
+        print("This setup.py requeries setuptools>=0.7\n")
+        sys.exit(0)
 from setuptools import setup, find_packages
 
 readme = open(os.path.join(os.path.dirname(__file__), 'README'))
@@ -15,8 +21,9 @@ setup(name='python-odesk3',
       author='oDesk',
       author_email='python@odesk.com',
       maintainer='Volodymyr Hotsyk',
+      use_2to3 = True,
       maintainer_email='gotsyk@gmail.com',
-      install_requires=['oauth2',],
+      install_requires=['oauth2', 'mock', 'nose'],
       dependency_links = ['https://github.com/hades/python-oauth2/tarball/python3#egg=oauth2',],
       packages=find_packages(),
       license = 'BSD',
